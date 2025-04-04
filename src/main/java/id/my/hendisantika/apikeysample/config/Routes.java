0b04a29fb0fb8a8060ca4238ec03eea93e30ee6a
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 06:38:30 +0700

    feat: Add Routes🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/apikeysample/config/Routes.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.apikeysample.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-apikey-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 05/04/25
+ * Time: 06.38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Routes {
+
+    public static final String QUOTES = "/api/quotes";
+    public static final String WEB_INDEX = "/web/home";
+}
