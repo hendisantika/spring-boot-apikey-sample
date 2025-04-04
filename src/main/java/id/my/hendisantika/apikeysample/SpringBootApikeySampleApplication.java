8a40c9dd5abdc6541c7052cb0e99cd001a40384f
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 06:31:42 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/apikeysample/SpringBootApikeySampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.apikeysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApikeySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApikeySampleApplication.class, args);
+    }
+
+}
