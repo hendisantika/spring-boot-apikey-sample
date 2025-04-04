90bae10c3f8f2f3b42094f8a8092b3c032b8216c
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 06:52:37 +0700

    feat: QuoteControllerIntegrationTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/apikeysample/controller/QuoteControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.apikeysample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-apikey-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 05/04/25
+ * Time: 06.52
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class QuoteControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+}
