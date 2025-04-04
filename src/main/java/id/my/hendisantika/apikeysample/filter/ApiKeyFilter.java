55a0f4c89fe16b90544a886223963b3adfcdcea3
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 06:35:59 +0700

    feat: Add doFilterInternal in ApiKeyFilter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/apikeysample/filter/ApiKeyFilter.java
+++ src/main/java/id/my/hendisantika/apikeysample/filter/ApiKeyFilter.java
@@ -1,9 +1,17 @@
 package id.my.hendisantika.apikeysample.filter;
 
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +34,28 @@ public class ApiKeyFilter extends OncePerRequestFilter {
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         var path = request.getRequestURI();
         return !path.startsWith(config.getPathPrefix());
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
+            ServletException, IOException {
+
+        String reqApiKey = request.getHeader("Api-Key");
+        boolean isApiKeyValid = authServiceHelper.validateApiKey(reqApiKey);
+
+        if (!isApiKeyValid) {
+            //return 401 Unauthorized
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid API Key");
+            return;
+        }
+
+        //apiKey is valid. Signal to Spring Security, this is an authenticated request
+        var authenticationToken = new UsernamePasswordAuthenticationToken(reqApiKey,
+                reqApiKey, Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        //continue process the request
+        filterChain.doFilter(request, response);
+
     }
 }
